6e224b459b1c9307c9c869b19f059945a31500e3
Mohamed-Fadel222 <116431641+Mohamed-Fadel222@users.noreply.github.com>
2024-04-14 02:10:42 +0200

    Implementing Register

--- Register.java
+++ Register.java
@@ -1,18 +1,47 @@
-public class Register
-{
+import java.util.Scanner;
 
-    /*
-    TODO
-     Register()
-     {
-     make instance of customer using constructor
-     manually assign accounttype, make initialbalance = 0 , log is empty by default.
-     add to both customers array and logininfo hashmap.
-     ID is set using the ID setter function
+public class Register {
+    
+    public Register() {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt the user for customer information
+        System.out.println("Enter first name:");
+        String FName = scanner.nextLine();
+        
+        System.out.println("Enter last name:");
+        String LName = scanner.nextLine();
+        
+        System.out.println("Enter username:");
+        String username = scanner.nextLine();
+        
+        System.out.println("Enter password:");
+        String password = scanner.nextLine();
+        
+        System.out.println("Enter phone number:");
+        String phoneNo = scanner.nextLine();
+        
+        System.out.println("Enter address:");
+        String address = scanner.nextLine();
 
-
-
+        System.out.println("Enter account type:");
+        String accountType = scanner.nextLine();
+        
+        System.out.println("Enter initial balance:");
+        double initialBalance = scanner.nextDouble();
+        
+        
+        
+        BankAccount account = new BankAccount(accountType, initialBalance);
+        Customer newCustomer = new Customer(FName, LName, username, password, phoneNo, address, accountType, initialBalance);
+        
+        
+        Customer.customers.add(newCustomer);
+        
+       
+        Customer.logininfo.put(username, password);
+        
+        
+        newCustomer.setID(newCustomer.getID());
     }
-
-     */
 }
