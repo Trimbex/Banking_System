48dc6570a88bd1a4b42fef88a34fb9b2e81754b9
“youssef123tt” <125928226+youssef123tt@users.noreply.github.com>
2024-04-19 21:05:27 +0200

    homeform

---
+++ inquiryform.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class inquiryform {
+    private JButton button1;
+    private JPanel inquiry;
+    private JLabel balance;
+
+    JFrame dep = new JFrame();
+
+    Customer customer;
+    inquiryform(){
+        dep.setSize(600,600);
+        dep.setVisible(true);
+        dep.setContentPane(inquiry);
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public void check(){
+        double balanceAmount = customer.getAccount().getInitialBalance();
+        balance.setText("Balance: " + balanceAmount);
+        balance.setText(String.valueOf(balanceAmount));
+
+    }
+
+
+}
